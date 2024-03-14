343c75894c692cbe39b64f20d831a9c79a0baf54
NaNahirr <nadianahir3264@gmail.com>
2024-03-14 14:44:42 -0300

    agrego otro número a la lista

--- src/proyectocompartido/ProyectoCompartido.java
+++ src/proyectocompartido/ProyectoCompartido.java
@@ -3,10 +3,11 @@ package proyectocompartido;
 public class ProyectoCompartido {
 
     public static void main(String[] args) {
-        int[] numeros = {1, 2, 3, 4};   
+        int[] numeros = {1, 2, 3, 4, 5};   
          
         for (int i = 0; i < numeros.length; i++) {
-            System.out.println(numeros[i]);  }                 
+            System.out.println(numeros[i]);
+        }                 
     }
     
 }
