f8d5d3b742c7fcc5c5e22e8c66479b7878df14df
NaNahirr <nadianahir3264@gmail.com>
2024-03-14 14:39:53 -0300

    creo primer proyecto compartido

---
+++ src/proyectocompartido/ProyectoCompartido.java
@@ -0,0 +1,12 @@
+package proyectocompartido;
+
+public class ProyectoCompartido {
+
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4};   
+         
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);  }                 
+    }
+    
+}
