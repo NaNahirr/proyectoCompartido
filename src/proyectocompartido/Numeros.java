638b751ba2e0846373b0c313fa3eb3abec8a3ae2
Gonzalonoe <gonza2002noe@gmail.com>
2024-03-19 17:18:41 -0300

    Numeros
    Gets and sets de numeros

---
+++ src/proyectocompartido/Numeros.java
@@ -0,0 +1,29 @@
+
+package proyectocompartido;
+
+public class Numeros {
+    public int num1;
+    public int num2;
+
+    public Numeros(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+    
+}
