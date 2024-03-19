e935e0abcf20be6ee6291dd53b2d5d7cf56ca29e
NaNahirr <nadianahir3264@gmail.com>
2024-03-19 16:58:04 -0300

    modifico alguna cosa

--- src/proyectocompartido/ProyectoCompartido.java
+++ src/proyectocompartido/ProyectoCompartido.java
@@ -6,7 +6,7 @@ public class ProyectoCompartido {
         int[] numeros = {1, 23, 3, 44};   
          
         for (int i = 0; i < numeros.length; i++) {
-            System.out.println(numeros);
+            System.out.println(numeros[i]);
         }                 
     }
     
